1395a9fe435dc85f8fcb7bd4a923c220ddbd111c
FarzadAkj <farzad2116@gmail.com>
2018-08-18 16:11:55 +0430

    empty project

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Hello world!
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
