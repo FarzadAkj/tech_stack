80004785b57f62924452ead0abe68e1c604dd76c
FarzadAkj <farzad2116@gmail.com>
2018-08-19 09:32:14 +0430

    redux boiler plate is set

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
 
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>
-          Hello world!
-        </Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View style={styles.container}>
+          <Text>
+            Hello world!
+          </Text>
+        </View>
+      </Provider>
     );
   }
 }
